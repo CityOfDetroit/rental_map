cf76822056a89d91374cc9d8214215eabfa51a48
Edgar Montes <jedgar1mx@gmail.com>
2022-02-24 15:43:43 -0500

    Adding travis pipeline.

---
+++ cypress/integration/initialLoad.js
@@ -0,0 +1,21 @@
+describe('Test map functionality', () => {
+    it('successfully loads', () => {
+      cy.visit('/') // change URL to match your dev URL
+      cy.contains('Welcome to the City of Detroit Rental Map')
+    })
+
+    it('test map click', () => {
+      cy.visit('/') // change URL to match your dev URL
+      cy.contains('Welcome to the City of Detroit Rental Map')
+      cy.get('#close-welcome').click()
+      cy.get('canvas').click('center')
+    })
+
+    it('enter address in geocoder', () => {
+      cy.visit('/') // change URL to match your dev URL
+      cy.contains('Welcome to the City of Detroit Rental Map')
+      cy.get('#close-welcome').click()
+      cy.wait(3000)
+      cy.get('#geocoder input').type('1104 military{enter}')
+    })
+})
